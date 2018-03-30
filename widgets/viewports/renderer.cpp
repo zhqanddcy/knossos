443e139faa6dfd3c64c73fb8348d0218c2696e8b
al3xst <keks@al3xst.de>
2018-03-30 16:11:01 +0200

    [WIP] First sphere with shader
    Color picking not working, spheres dont scale with zoom, sphere size and sphere color is const, depth/gradient is wrong

--- widgets/viewports/renderer.cpp
+++ widgets/viewports/renderer.cpp
@@ -2217,12 +2217,11 @@ void ViewportBase::renderSkeleton(const RenderOptions &options) {
     glEnableClientState(GL_VERTEX_ARRAY);
     glEnableClientState(GL_COLOR_ARRAY);
 
-//    /* draw all nodes */
+    /* draw all nodes */
     state->viewerState->pointVertBuffer.vertex_buffer.bind();
-    glVertexPointer(3, GL_FLOAT, 0, nullptr);
-//    const int vertexLocation = sphereShader.attributeLocation("vertex");
-//    sphereShader.enableAttributeArray(vertexLocation);
-//    sphereShader.setAttributeBuffer(vertexLocation, GL_FLOAT, 0, 3);
+    const int vertexLocation = sphereShader.attributeLocation("vertex");
+    sphereShader.enableAttributeArray(vertexLocation);
+    sphereShader.setAttributeBuffer(vertexLocation, GL_FLOAT, 0, 3);
     state->viewerState->pointVertBuffer.vertex_buffer.release();
 
     if(options.nodePicking) {
@@ -2239,7 +2238,7 @@ void ViewportBase::renderSkeleton(const RenderOptions &options) {
 //        state->viewerState->pointVertBuffer.color_buffer.release();
     }
 
-    //    glTranslatef(0.5, 0.5, 0.5);
+    glEnable(GL_VERTEX_PROGRAM_POINT_SIZE);
 
     GLfloat modelview_mat[4][4];
     glGetFloatv(GL_MODELVIEW_MATRIX, &modelview_mat[0][0]);
@@ -2256,9 +2255,11 @@ void ViewportBase::renderSkeleton(const RenderOptions &options) {
 
     glDrawArrays(GL_POINTS, 0, static_cast<GLsizei>(state->viewerState->pointVertBuffer.vertices.size()));
 
-//    sphereShader.disableAttributeArray(vertexLocation);
+    sphereShader.disableAttributeArray(vertexLocation);
 
     sphereShader.release();
+
+    glDisable(GL_VERTEX_PROGRAM_POINT_SIZE);
 
     glDisableClientState(GL_COLOR_ARRAY);
     glDisableClientState(GL_VERTEX_ARRAY);
